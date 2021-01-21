638f5cced55d8aee07e09bffc48d5d96b4adf98d
Your NameIrina-S <serysheva.i@yandex.ru>
2021-01-21 23:38:34 +0900

    Добавленна модальное окно

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,18 +1,10 @@
-// JS-функция определения поддержки WebP
-// function testWebP(callback) {
+$(document).ready(function(){
+    $('a[href="#modal-request"]').fancybox({
+        'overlayShow'   :   true
+    });
 
-//     var webP = new Image();
-//     webP.onload = webP.onerror = function () {
-//     callback(webP.height == 2);
-//     };
-//     webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-//     }
-    
-//     testWebP(function (support) {
-    
-//     if (support == true) {
-//     document.querySelector('body').classList.add('webp');
-//     }else{
-//     document.querySelector('body').classList.add('no-webp');
-//     }
-//     });
+    $('.modal-cross').on('click', function(e){
+        e.preventDefault();
+        $.fancybox.close();
+    });
+})
